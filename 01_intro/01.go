5c3c5906695c3b772cfd6dc3762c40d73ee43728
kelvinokumu <oumakelvinokumu@gmail.com>
2023-05-27 11:16:42 +0300

    handling variables

---
+++ 01_intro/01.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	var price int = 2
+	fmt.Println(price)
+
+	var number = 150
+	fmt.Println(number)
+	fmt.Printf("%T \n", number)
+
+	number_1 := 65
+	fmt.Println(number_1)
+	fmt.Printf("%T \n", number_1) // Printf gives you the type of the variable
+}
