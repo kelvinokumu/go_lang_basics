e1b126d60023fa0c305410ad2580dc6dd35db72c
kelvinokumu <oumakelvinokumu@gmail.com>
2023-05-27 11:17:29 +0300

    data types

---
+++ 01_intro/02.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	price := 25.5
+	fmt.Printf("%T \n", price)
+
+	state := false
+	fmt.Printf("%T \n", state)
+
+	country := "kenya"
+	fmt.Printf("%T \n", country)
+
+	letter := "k"
+	fmt.Printf("%T \n", letter)
+}
