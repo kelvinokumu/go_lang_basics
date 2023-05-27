7465698e3a3e718e182d1ee63164015e8ed338ba
kelvinokumu <oumakelvinokumu@gmail.com>
2023-05-27 11:17:55 +0300

    default values

---
+++ 01_intro/03.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// defualt is 0
+	var number int64
+	fmt.Println(number)
+
+	// default is 0
+	var price float32
+	fmt.Println(price)
+
+	// default is false
+	var state bool
+	fmt.Println(state)
+
+	// default is empty
+	var country string
+	fmt.Println(country)
+}
