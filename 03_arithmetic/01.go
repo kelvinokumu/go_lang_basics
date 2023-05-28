c1f0c5af67231343026a78f28e5e144b7acfbf9f
kelvinokumu <oumakelvinokumu@gmail.com>
2023-05-28 11:06:03 +0300

    arithmetic operator

--- 03_arithmetic/01.go
+++ 03_arithmetic/01.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Check")
+	fmt.Println("Start")
 	num1 := 24
 	num2 := 5
-	sum := num1 % num2
+	sum := num1 + num2
+	sub := num1 - num2
+	mult := num1 * num2
+	div := num1 / num2
+	modulo := num1 % num2
 	fmt.Println(sum)
+	fmt.Println(sub)
+	fmt.Println(div)
+	fmt.Println(mult)
+	fmt.Println(modulo)
 }
