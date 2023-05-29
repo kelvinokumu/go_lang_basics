32f2984ad979048b2188504b7591b841ba84bf4d
kelvinokumu <oumakelvinokumu@gmail.com>
2023-05-29 10:11:10 +0300

    arithmetic

--- 03_arithmetic/01.go
+++ 03_arithmetic/01.go
@@ -4,13 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Start")
+
 	num1 := 24
 	num2 := 5
+
 	sum := num1 + num2
 	sub := num1 - num2
 	mult := num1 * num2
 	div := num1 / num2
 	modulo := num1 % num2
+
 	fmt.Println(sum)
 	fmt.Println(sub)
 	fmt.Println(div)
