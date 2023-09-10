1e876cdbf34b69233fa7822bd9f50114132b2336
kelvinokumu <oumakelvinokumu@gmail.com>
2023-09-10 22:45:43 +0300

    go code

--- 01_intro/01.go
+++ 01_intro/01.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// // how to declare variables
-	// fmt.Println("Hello, World!")
-	// var price int = 2 // 1
-	// fmt.Println(price)
+	// how to declare variables
+	fmt.Println("Hello, World!")
+	var price int = 2 // 1
+	fmt.Println(price)
 
-	// var number = 150.77 // 2
-	// fmt.Println(number)
-	// fmt.Printf("The type is %T \n", number)
+	var number = 150.77 // 2
+	fmt.Println(number)
+	fmt.Printf("The type is %T \n", number)
 
-	// number_1 := 65 // 3
-	// fmt.Println(number_1)
-	// fmt.Printf("The type is %T \n", number_1) // Printf gives you the type of the variable
+	number_1 := 65 // 3
+	fmt.Println(number_1)
+	fmt.Printf("The type is %T \n", number_1) // Printf gives you the type of the variable
 	odd()
 
 }
